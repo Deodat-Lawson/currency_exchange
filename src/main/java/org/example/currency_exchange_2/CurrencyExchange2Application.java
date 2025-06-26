36f64000b1c52c3dcc5be0464871b93f2ce53794
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2025-06-25 23:23:39 -0700

    init

---
+++ src/main/java/org/example/currency_exchange_2/CurrencyExchange2Application.java
@@ -0,0 +1,13 @@
+package org.example.currency_exchange_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchange2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CurrencyExchange2Application.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/currency_exchange_2/CurrencyExchange2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.currency_exchange_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchange2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
