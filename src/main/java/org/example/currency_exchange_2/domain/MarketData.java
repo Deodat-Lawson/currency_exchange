ace05aa12ba69af0b45d042a55820998485e0f4b
Timothy Lin <55767165+Deodat-Lawson@users.noreply.github.com>
2025-07-12 02:38:08 -0700

    Added in reasonable initial values

--- src/main/java/org/example/currency_exchange_2/domain/MarketData.java
+++ src/main/java/org/example/currency_exchange_2/domain/MarketData.java
@@ -5,21 +5,19 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
-//TODO: Add in reasonable initial values for all the variables if its not provided in the parameters
-
 @Data
 @NoArgsConstructor
 @Validated
 public class MarketData {
   @Min(0)
-  private Integer exchangeId;
+  private Integer exchangeId = 1;
   @NotBlank
   private String quote;
   @NotBlank
   private String base;
 
-  private Long startTime;
-  private Long endTime;
+  private Long startTime = System.currentTimeMillis() - (60 * 60 * 1000L);
+  private Long endTime = System.currentTimeMillis();
 
   //Mock data value to test controller
   public MarketData(Integer exchangeId, String quote, String base, Long startTime, Long endTime){
